5ae2dbe21710f668a83bdcce6de192d7b5b8b5b5
EmilioRCPantojaLnx <emiliorafael.pantoja46@gmail.com>
2025-09-24 11:59:44 -0300

    aula 07 introdução a javascript e sua junção com CSS

---
+++ aula07/script.js
@@ -0,0 +1,9 @@
+alert("eu quero")
+confirm("quer ?")
+prompt("eu ainda quero", "me diz se ainda quer")
+fechaJanela()
+    
+function fechaJanela(){
+    confirm("fechar a janela?")?window.close():null
+}
+
